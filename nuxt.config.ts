d721cff55b9907dd0468dba21fe0e255e2961857
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2026-02-25 01:08:39 +0100

    Add Vatly favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,8 +13,25 @@ export default defineNuxtConfig({
       link: [
         {
           rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico',
+        },
+        {
+          rel: 'icon',
           type: 'image/png',
-          href: '/vatly-logo-square.png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
         },
         {
           rel: 'preconnect',
