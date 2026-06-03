131a0e40b19f48213c6d8ecf0368b189b0fe3fe2
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2026-06-03 22:44:50 +0000

    Updates

---
+++ _nuxt/B1WzmH3i.js
@@ -0,0 +1,1 @@
+import{r as n}from"./CHhXdjqG.js";import a from"./BoDWAUc2.js";import{$ as s,a as t,p as e,s as i,o as p}from"./DtxkQMgf.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(p(),t(a,{color:"warning",icon:i(o).ui.icons.warning},{default:e(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
