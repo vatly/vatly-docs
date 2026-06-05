9191a5fd4afffb46a56fbee64caa7d0e76d3afaa
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2026-06-05 18:02:31 +0000

    Updates

--- _nuxt/BjpIz4bX.js
+++
@@ -1,1 +0,0 @@
-import{r as n}from"./DhcvVk3c.js";import a from"./Dn2Mz70l.js";import{$ as s,a as t,p as e,s as i,o as p}from"./BJk1sKHS.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(p(),t(a,{color:"warning",icon:i(o).ui.icons.warning},{default:e(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ _nuxt/CaGkcgB7.js
@@ -0,0 +1,1 @@
+import{r as n}from"./Q8yZ5swn.js";import a from"./D8pf5oM_.js";import{$ as s,a as t,p as e,s as i,o as p}from"./iP20PRGU.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(p(),t(a,{color:"warning",icon:i(o).ui.icons.warning},{default:e(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
