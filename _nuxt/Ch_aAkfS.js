8041b25beadb1abde885e2b3f7d42cc5f04b1fb8
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2026-06-05 18:01:44 +0000

    Updates

--- _nuxt/Ch_aAkfS.js
+++
@@ -1,1 +0,0 @@
-import{r as n}from"./0hmKU2gs.js";import a from"./CR30El17.js";import{$ as s,a as t,p as e,s as i,o as p}from"./YRl9WQmV.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(p(),t(a,{color:"warning",icon:i(o).ui.icons.warning},{default:e(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
