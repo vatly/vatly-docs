5d6a2ba8b527694f8f3fb70a758ee838eedb629d
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2026-06-24 14:36:09 +0000

    Updates

--- _nuxt/BQqn9nJD.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./CdFxJhEB.js";import t from"./DfS2Cprh.js";import{$ as a,a as e,p,s as c,o as n}from"./BVaM79h-.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=a();return(s,f)=>(n(),e(t,{color:"success",icon:c(o).ui.icons.tip},{default:p(()=>[r(s.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
--- _nuxt/yeia3FiU.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./CqpCMfuc.js";import{bV as a,a as n,p as s,q as r,o as c}from"./BVaM79h-.js";const _={};function f(t,l){const e=o;return c(),n(e,null,{default:s(()=>[r(t.$slots,"default")]),_:3})}const m=a(_,[["render",f]]);export{m as default};
